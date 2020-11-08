a8c2060922a4cbab09b140a9589000c85f09d8f0
teleskop15070 <teleskop150750@gmail.com>
2020-11-08 18:21:47 +0300

    Вывод точки на экран

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,23 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Drow(x1, y1, c1);
+
+            Console.ReadLine();
+        }
+
+        private static void Drow(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
+
 }
